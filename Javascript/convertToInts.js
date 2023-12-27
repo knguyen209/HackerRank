c4e72c73e97a9a253d8d80b93e958859408963a5
Kenny Nguyen <hien.p.nguyen01@sjsu.edu>
2023-12-27 14:04:38 -0800

    New solutions

--- Javascript/convertToInts.js
+++ Javascript/convertToInts.js
@@ -1,9 +1,9 @@
 function convertToIntegers(strings) {
     if (strings !== null) {
-        return strings.map((n) => parseInt(n));
-    } else return null;
+        return strings.map((n) => parseInt(n))
+    } else return null
 }
 
-console.log(convertToIntegers(["1", "2", "3"]));
-console.log(convertToIntegers([]));
-console.log(convertToIntegers(null));
+console.log(convertToIntegers(['1', '2', '3']))
+console.log(convertToIntegers([]))
+console.log(convertToIntegers(null))
--- Javascript/pairs.js
+++ Javascript/pairs.js
@@ -1,13 +1,27 @@
 // https://www.hackerrank.com/challenges/pairs/problem
 function pairs(k, arr) {
     let total = 0
-
-    for (let i = 0; i < arr.length; i++) {
-        let targetVal = arr[i] + k
-        if (arr.includes(targetVal)) {
-            total++
-        }
+    let sortedArr = arr.sort((a, b) => a - b)
+    for (let i = 0; i < sortedArr.length; i++) {
+        binarySearch(sortedArr[i] + k, sortedArr, 0, sortedArr.length - 1) !=
+            -1 && total++
     }
 
     return total
+}
+
+function binarySearch(key, arr, startIndex, endIndex) {
+    if (startIndex <= endIndex) {
+        let mid = startIndex + parseInt((endIndex - startIndex) / 2)
+        if (key < arr[mid]) {
+            return binarySearch(key, arr, startIndex, mid - 1)
+        }
+        if (key > arr[mid]) {
+            return binarySearch(key, arr, mid + 1, endIndex)
+        }
+        if (key == arr[mid]) {
+            return mid
+        }
+    }
+    return -1
 }
